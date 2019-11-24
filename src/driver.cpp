b1b82b08c0f24ac32f5f21fd193a0f41fb0a47b6
Eduardo Sarmento <cealvesarmento@gmail.com>
2019-11-24 20:06:52 -0300

    Código do livro implementado. Revisão necessária

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,9 +1,27 @@
 #include "../include/grace.hpp"
+#include "../include/pointer.hpp"
+
+#include <new>
 
 using namespace grace;
 
 int main () {
-    MemoryPool<24> * memory = new MemoryPool<24>(1024);
+    // MemoryPool<24> * memory = new MemoryPool<24>(1024);
+
+    GracePointer<int> p;
+    
+    try {
+        p = new int;
+    } catch (bad_alloc e) {
+        std::cout << "Falha na alocação" << std::endl;
+        return 1;
+    }
+
+    *p = 88;
+
+    cout << "Valor de p é: " << *p << std::endl;
+    int k = *p;
+    cout << "k é " << k << std::endl;
 
     return 0;
 }
